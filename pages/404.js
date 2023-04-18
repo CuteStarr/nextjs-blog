ff0ad6af09c92ac3f9780facf472e833756eb55c
unknown <jimmie.deepmm@gmail.com>
2023-04-17 18:46:37 -0700

    initial

---
+++ pages/404.js
@@ -0,0 +1,3 @@
+export default function Custom404() {
+    return <h1>Hey stone head! type right!</h1>;
+}
---
+++ pages/500.js
@@ -0,0 +1,3 @@
+export default function Custom500() {
+    return <h1>Typeing right now, stupid</h1>
+}
